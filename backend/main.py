1687ca1a5ae597bcb81ec4ed4efb830d2be54fda
Calvin Rea <calvinrea123@gmail.com>
2025-07-02 11:29:43 +0200

    Added backend functionality for retrieving images of people being shot

--- backend/main.py
+++ backend/main.py
@@ -75,6 +75,10 @@ async def delete_user(user_id: int):
     except HTTPException as e:
         print(e)
         return {"message": f"User with user_id:{user_id} not found"}
+    
+@app.get("/admin/images")
+async def get_images():
+    return list_recent_images()
 
 @app.websocket("/ws/{user_id}")
 async def websocket_endpoint(websocket: WebSocket, user_id: int):
@@ -85,7 +89,6 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int):
         try:
            while True:
                 data = await websocket.receive_text()
-                # TODO: broadcast killer:player object,target:player object
                 target_id = process_shot(data)
                 if target_id is int:
                     shot_obj = {'killer': get_user(user_id), 'target':get_user(target_id)}
